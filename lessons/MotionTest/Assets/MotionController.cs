d7a2a92ab7c10ba199eb9279261dce560926f0d3
samnau <samgnaw77@gmail.com>
2014-12-09 12:25:44 -0600

    created simple and short 4 directional character controller

---
+++ lessons/MotionTest/Assets/MotionController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionController : MonoBehaviour {
+	public float motionDistance = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * motionDistance * Time.deltaTime;
+	}
+}
