709caaee6e681b8ebe8e2627da799c52f9845a10
samnau <samgnaw77@gmail.com>
2014-12-15 12:58:24 -0600

    added collision detection and player destruction on hit

--- lessons/MotionTest/Assets/MotionController.cs
+++ lessons/MotionTest/Assets/MotionController.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 public class MotionController : MonoBehaviour {
-	public float motionDistance = 1.0f;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+	public float motionDistance = 1.0f;
 	// Update is called once per frame
 	void Update () {
 		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 		transform.position += move * motionDistance * Time.deltaTime;
 	}
+
 }
