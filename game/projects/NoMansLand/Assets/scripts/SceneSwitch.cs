8747161276acf46fd3749b148b8fa4746d3ff9d2
samnau <samgnaw77@gmail.com>
2015-03-18 22:04:54 -0500

    added colliders and basic scene triggers

--- game/projects/NoMansLand/Assets/scripts/SceneSwitch.cs
+++ game/projects/NoMansLand/Assets/scripts/SceneSwitch.cs
@@ -2,12 +2,17 @@
 using System.Collections;
 
 public class SceneSwitch : MonoBehaviour {
-
+	public string sceneName;
 	// Use this for initialization
 	void Start () {
-		Application.LoadLevel ("Scene2"); 
+		//Application.LoadLevel ("Scene2"); 
 	}
-	
+	void OnTriggerEnter2D( Collider2D other ){
+		if(other.CompareTag("hero")) {
+				Debug.Log("Forest1 Up");
+				Application.LoadLevel(sceneName);
+		}
+	}
 	// Update is called once per frame
 	void Update () {
 	
