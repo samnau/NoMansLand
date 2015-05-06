556586b4d972ee19578f05549a6cac3dcac21fe3
samnau <samgnaw77@gmail.com>
2015-05-06 13:02:12 -0500

    added code to make animation respond to direction pressed

--- game/projects/NoMansLand/Assets/scripts/MotionController.cs
+++ game/projects/NoMansLand/Assets/scripts/MotionController.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class MotionController : MonoBehaviour {
 	public GameObject Player;
-
+	private float X;
 	// Use this for initialization
 	void Start () {
-
+		X = transform.localScale.x;
 	}
 	private bool isMoving(){
 		return Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right");
@@ -20,6 +20,15 @@ public class MotionController : MonoBehaviour {
 	void Update () {
 		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 		transform.position += move * motionDistance * Time.deltaTime;
+
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			transform.localScale = new Vector3(X,transform.localScale.y,transform.localScale.z);
+		}
+		else if (Input.GetAxis("Horizontal") > 0)
+		{
+			transform.localScale = new Vector3(-X,transform.localScale.y,transform.localScale.z);
+		}
 
 		if (isMoving()) {
 			GetComponent<Animator>().SetBool("isWalking",true);
