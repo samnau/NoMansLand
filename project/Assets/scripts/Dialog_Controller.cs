c618da0be7f8ceee6c4815d64ff253a680b91bc4
Sam Nau <sam.nau@vml.com>
2016-02-12 12:21:18 -0600

    start of conversation component - completed working example of gameObject communication

--- project/Assets/scripts/Dialog_Controller.cs
+++ project/Assets/scripts/Dialog_Controller.cs
@@ -4,11 +4,15 @@ using System.Collections;
 //private Animator animationController;
 
 public class Dialog_Controller : MonoBehaviour {
-private Animator animationController;
+	private Animator animationController;
+	public GameObject speakerLeft;
 	// Use this for initialization
 	void Start () {
-		animationController = GetComponent<Animator> ();
-		animationController.SetBool("right",true);
+		//animationController = GetComponent<Animator> ();
+		//animationController.SetBool("right",true);
+		speakerLeft.GetComponent<Animator> ().SetBool("right",true); 
+		 //Debug.Log(speaker);
+		 //.GetComponent.<Animator>().setBool("right", true);
 	}
 
 	// Update is called once per frame
