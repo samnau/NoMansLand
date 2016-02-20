a2f4542266872f8a8b66e57f77e669fa685fc149
Sam Nau <sam.nau@vml.com>
2016-02-20 17:12:37 -0600

    start of conversation component - completed working example of finding gameObject by name and changing it

---
+++ project/Assets/scripts/ExampleScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent (typeof (Rigidbody))]
+public class ExampleScript : MonoBehaviour {
+	GameObject myGameObject;
+	Transform myTransform;
+	private Rigidbody myRigidBody;
+	// Use this for initialization
+	void Start () {
+		//myRigidBody = GetComponent<Rigidbody> ();
+		myGameObject = GameObject.Find("Jack");
+		myTransform = myGameObject.GetComponent<Transform> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		myTransform.position = Vector3.up * Time.time;
+		//myTransform.position= Vector3.up * Time.time;
+		//myRigidBody.AddForce(Vector3.up * 50);
+	}
+}
