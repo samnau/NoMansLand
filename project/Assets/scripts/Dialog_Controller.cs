80339ecd7652b11fb55f17f32b26173f9633a8aa
Sam Nau <sam.nau@vml.com>
2016-02-22 12:31:45 -0600

    conversation component - simplified speaker control trigger into a single reusable function

--- project/Assets/scripts/Dialog_Controller.cs
+++ project/Assets/scripts/Dialog_Controller.cs
@@ -4,24 +4,21 @@ using System.Collections;
 //private Animator animationController;
 
 public class Dialog_Controller : MonoBehaviour {
-	Animator animator_left;
-	Animator animator_right;
-	GameObject speakerLeft;
-	GameObject speakerRight;
+
 	// Use this for initialization
 	void Start () {
-		speakerLeft = GameObject.FindGameObjectWithTag ("Speaker_LEFT");
-		speakerRight = GameObject.FindGameObjectWithTag ("Speaker_RIGHT");
-		animator_left = speakerLeft.GetComponent<Animator> ();
-		animator_left.SetBool ("right", true);
-		animator_right = speakerRight.GetComponent<Animator> ();
-		animator_right.SetBool ("left", true);
-
-		//animationController = GetComponent<Animator> ();
-		//animationController.SetBool("right",true);
-		//speakerLeft.GetComponent<Animator> ().SetBool("right",true); 
-		 //Debug.Log(speaker);
-		 //.GetComponent.<Animator>().setBool("right", true);
+		TriggerSpeaker ("LEFT");
+		TriggerSpeaker ("RIGHT");
+	}
+	void TriggerSpeaker(string speakerTarget){
+		Animator targetAnimator;
+		GameObject targetObject;
+		string tagString;
+		string direction = speakerTarget == "RIGHT" ? "left" : "right";
+		tagString = "Speaker_" + speakerTarget;
+		targetObject = GameObject.FindGameObjectWithTag (tagString);
+		targetAnimator = targetObject.GetComponent<Animator> ();
+		targetAnimator.SetBool (direction, true);
 	}
 
 	// Update is called once per frame
