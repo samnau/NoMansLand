5bf708ce2856b3e65e89a252a38822369dfa3cda
Sam Nau <sam.nau@vml.com>
2016-04-22 13:51:39 -0500

    multi line  auto typer script test

---
+++ project/Assets/scripts/CoroutineChainTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChainedEventsManager : MonoBehaviour {
+
+	public int EventId;    
+	public List<string> ChainedEvents;
+	public bool IsExecuting = false;
+	//    
+	public void NextTask(int nextId)
+	{
+		EventId = nextId;
+		//audio.Play();
+	}
+
+	void Update()
+	{
+		if(EventId >= 0 && !IsExecuting)
+		{ 
+			IsExecuting = true;
+			//Execute Next chained action          
+			SendMessageUpwards(ChainedEvents[EventId]);     
+			//if is the last item on the chain, set the event id to -1 so the cycle is not hit anymore
+			if (EventId == ChainedEvents.Count)
+			{
+				EventId = -1;
+			}
+
+		}
+	}
+}
--- project/Assets/scripts/TextImporter.cs
+++ project/Assets/scripts/TextImporter.cs
@@ -17,15 +17,27 @@ public class TextImporter : MonoBehaviour {
 		} else if (testText2) {
 			textLines = testText2.text.Split ('\n');
 		}
-		StartCoroutine ("TypeLetters");
+		StartCoroutine (TypeOutLines(testText1.text));
 
 		//textComponent.text = textLines [1];
 	}
 
-	public IEnumerator TypeLetters() 
+	IEnumerator TypeOutLines(string textLinesString){
+		var textArray = textLinesString.Split('\n');
+		for (int z = 0; z < textArray.Length; z++) 
+		{
+			IEnumerable TypeCoroutine = TypeLetters (textArray [z]);
+			yield return StartCoroutine( TypeCoroutine.GetEnumerator() );
+		}
+	}
+
+	IEnumerable TypeLetters(string textLine) 
 	{
-		var textValue = textLines [1];
+
+		var textValue = textLine;
 		//Debug.Log (textValue);
+		//var textArray = textLinesString.Split('\n');
+		//print(textArray);
 		for (int i = 0; i < textValue.Length; i++)
 		{
 			var currentText = textValue.Substring(0, i);
