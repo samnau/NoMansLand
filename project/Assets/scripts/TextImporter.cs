e55abc235496b49f98397277ae7c3464e835b5d5
Sam Nau <sam.nau@vml.com>
2016-05-11 13:42:05 -0500

    got basic speaker switching without state to work
    but trying to pick up where speaker left off caused a freeze

--- project/Assets/scripts/TextImporter.cs
+++ project/Assets/scripts/TextImporter.cs
@@ -8,14 +8,21 @@ public class TextImporter : MonoBehaviour {
 	public TextAsset testText2;
 	public string[] textLines;
 	public string targetScene;
-	int speaker1Counter = 0;
-	int speaker2Counter = 0;
-	int speaker3Counter = 0;
-	int speaker4Counter = 0;
+//	int speaker1Counter = 0;
+//	int speaker2Counter = 0;
+//	int speaker3Counter = 0;
+//	int speaker4Counter = 0;
+	Dictionary <string, int> speakerCounters = new Dictionary <string, int>{
+		{ "SPEAKER_1", 0 },
+		{ "SPEAKER_2", 0 },
+		{ "SPEAKER_3", 0 },
+		{ "SPEAKER_4", 0 }
+	};
+	string currentSpeakerName = "SPEAKER_1";
 	string currentTextString;
 	string[] targetTextArray;
 
-	int targetSpeaker  = 1;
+	string targetSpeaker  = "1";
 	GameObject targetTextBox;
 	GameObject storyManager;
 	StoryTextManager storyManagerController;
@@ -32,33 +39,62 @@ public class TextImporter : MonoBehaviour {
 		Debug.Log (targetStoryData.TEXT_1);
 		parseTargetSpeakerText ();
 		textComponent = targetTextBox.GetComponent<Text> ();
-		//StartCoroutine ()
-		StartCoroutine (TypeOutLines(currentTextString));
+		StartCoroutine (TypeOutLines());
 	}
 	void parseTargetSpeakerText (){
 		string targetProperty = "TEXT_" + targetSpeaker;
+		Debug.Log (targetProperty);
 		currentTextString = targetStoryData.GetType ().GetField (targetProperty).GetValue (targetStoryData) as string;
-//		targetTextArray = currentTextString.Split('*');
-//		Debug.Log (targetTextArray[0]);
 	}
 	void findStoryData(string targetSceneName){
 		targetStoryData = storyManagerController.Find_SCENENAME (targetSceneName);
 	}
-
-	IEnumerator TypeOutLines(string textLinesString){
-		var textArray = textLinesString.Split('*');
-		for (int z = 0; z < textArray.Length; z++)
+	void setTargetSpeakerIndex (string speakerName){
+		//var simpleName = speakerName.Substring (speakerName.IndexOf ("[") + 1, speakerName.IndexOf ("]") - 1);
+		setCurrentSpeakerName (speakerName);
+		Debug.Log (currentSpeakerName);
+		targetSpeaker = currentSpeakerName.Split ('_') [1];
+	}
+	IEnumerator TypeOutLines(){
+		var textArray = currentTextString.Split('*');
+		var startIndex = (speakerCounters [currentSpeakerName]);
+		//switching to startIndex caused a freeze after speaker switching
+		for (int z = startIndex; z < textArray.Length; z++)
 		{
 			IEnumerable TypeCoroutine = TypeLetters (textArray [z]);
-			yield return StartCoroutine( TypeCoroutine.GetEnumerator() );
-			yield return new WaitForSeconds(1.0f);
+			var isSwitchStatement = textArray [z].IndexOf("[") != -1;
+			Debug.Log (isSwitchStatement);
+			if(isSwitchStatement){
+				updateSpeakerState (z);
+				yield return StartCoroutine( SwitchSpeaker(textArray [z]) );
+				yield break;
+			}else{
+				yield return StartCoroutine( TypeCoroutine.GetEnumerator() );
+				yield return new WaitForSeconds(1.0f);
+			}
+
 		}
 	}
+	void updateSpeakerState (int currentIndex){
+		speakerCounters [currentSpeakerName] = currentIndex;
+	}
+	void setCurrentSpeakerName(string speakerName){
+		currentSpeakerName = speakerName.Substring (speakerName.IndexOf ("[") + 1, speakerName.IndexOf ("]") - 1);
+	}
+//	int findSpeakerIndex (string currentSpeakerName){
+//		return currentSpeakerName.Split ('_') [1];
+//	}
+	IEnumerator SwitchSpeaker(string newSpeaker){
 
+		setTargetSpeakerIndex (newSpeaker);
+		parseTargetSpeakerText ();
+		Debug.Log (currentTextString);
+		yield return StartCoroutine (TypeOutLines());
+		//yield return new WaitForSeconds(1.0f);
+	}
 	IEnumerable TypeLetters(string textLine)
 	{
 		var textValue = textLine;
-		//Debug.Log(textValue);
 		for (int i = 0; i < textValue.Length+1; i++)
 		{
 			var currentText = textValue.Substring(0, i);
