97435475bf3a8c5e86119b5b2e0bae1a835b9657
Sam Nau <samgnaw77@gmail.com>
2016-07-12 11:50:52 -0500

    updating to latest Unity (5.3)

---
+++ project/Assets/scripts/SpeakerMotionController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SpeakerMotionController : MonoBehaviour {
+	private Vector2 speaker_vector;
+
+	private GameObject speaker;
+	private Rigidbody2D targetObject;
+	private float moveDistance = 1.0f;
+	public string direction;
+	// Use this for initialization
+	void Start () {
+		speaker = GameObject.Find("Speaker");
+		targetObject = speaker.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate(){
+		targetObject.AddRelativeForce (Vector2.right * moveDistance);
+
+	}
+}
