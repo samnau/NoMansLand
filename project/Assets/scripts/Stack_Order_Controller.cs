a98974f681cd7a8f6fe27bdeba24dc18a39b006f
Sam Nau <2samnau@gmail.com>
2018-08-23 20:44:20 -0500

    * converted the stack order controller from using z postion to using sortingOrder to guarantee proper stacking * added new cloud motion controller with randomized size, position and speed

--- project/Assets/scripts/Stack_Order_Controller.cs
+++ project/Assets/scripts/Stack_Order_Controller.cs
@@ -3,15 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Stack_Order_Controller : MonoBehaviour {
-	//private float Xpos;
 	private float Ypos;
 	void StackSetter(){
-		//Xpos = transform.position.x;
 		Ypos = transform.position.y;
         var objectSorter = GetComponent<SpriteRenderer>();
         var newOrder = Mathf.RoundToInt(Ypos * -100f);
         objectSorter.sortingOrder = newOrder;
-        //transform.position = new Vector3(Xpos, Ypos, Ypos);
 	}
 
 	void Start () {
