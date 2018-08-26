1d7c0d7ea46f14a207da6e22b48d1a311b635b3f
Sam Nau <2samnau@gmail.com>
2018-08-26 12:51:52 -0500

    * added colliders to drawbridge and door * added scripted sequence to allow player to walk into castle after walls come down     * disabled the perspective controller after the walls come down * added scene switchers on the door and the bottom of the scene * updated Broken Pool top edge scene switcher to be the castle

--- project/Assets/scripts/Castle_Wall_Controller.cs
+++ project/Assets/scripts/Castle_Wall_Controller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Castle_Wall_Controller : MonoBehaviour {
     private GameObject castleWalls;
     private Camera mainCamera;
+    private bool wallsAreUp = true;
 	// Use this for initialization
 	void Start () {
 		castleWalls = GameObject.Find("walls_wrapper");
@@ -12,11 +13,25 @@ public class Castle_Wall_Controller : MonoBehaviour {
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && wallsAreUp)
         {
             castleWalls.GetComponent<Animator>().SetBool("lowerWalls", true);
             mainCamera.GetComponent<Camera_Shaker>().enabled = true;
+            StartCoroutine(WallSequence());
         }
+    }
+
+    IEnumerator WallSequence()
+    {
+        Debug.Log("next");
+        var hero = GameObject.FindGameObjectWithTag("Player");
+        //hero.GetComponent<PlayerMotionController>().enabled = false;
+        //hero.GetComponent<InputStateTracker>().isWalking = false;
+        yield return new WaitForSeconds(2.0f);
+        wallsAreUp = false;
+        //hero.GetComponent<PlayerMotionController>().enabled = true;
+        hero.GetComponent<Perspective_Scale_Controller>().enabled = false;
+        GameObject.Find("wall_blocker").GetComponent<BoxCollider2D>().enabled = false;
     }
     // Update is called once per frame
     void Update () {
