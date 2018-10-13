eaf1ab27b61db2563590b0009c6b0c17e8cf118e
Sam Nau <2samnau@gmail.com>
2018-10-12 22:33:22 -0500

    WIP commit for the next tree animations

--- project/Assets/scripts/TreeBreezeController.cs
+++ project/Assets/scripts/TreeBreezeController.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class TreeBreezeController : MonoBehaviour {
     private Animator animatonController;
+    public float breezeDelay = 0f;
+
 	// Use this for initialization
 	void Start () {
         animatonController = GetComponent<Animator>();
         StartCoroutine(TreeSequence());
     }
+
+    void controlWind(bool wind)
+    {
+        animatonController.SetBool("wind", wind);
+    }
+
+    void controlWindStrength(bool strong)
+    {
+        var windSpeed = strong ? 2.0f : 1.0f;
+        animatonController.SetBool("strong", strong);
+        animatonController.speed = windSpeed;
+    }
+
     IEnumerator TreeSequence()
     {
-        yield return new WaitForSeconds(3.0f);
-        animatonController.speed = 2.0f;
+        yield return new WaitForSeconds(breezeDelay);
+        //animatonController.speed = 2.0f;
+        controlWind(true);
+        yield return new WaitForSeconds(2.0f);
+        controlWindStrength(true);
         yield return new WaitForSeconds(5.0f);
-        animatonController.speed = 1.0f;
-        animatonController.SetBool("strong", false);
+        controlWindStrength(false);
         yield return new WaitForSeconds(1.0f);
-        animatonController.SetBool("wind", false);
-
+        controlWind(false);
     }
     // Update is called once per frame
     void Update () {
