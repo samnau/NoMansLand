93350e0e4a827cbf1d7dda0c717cf9ad0ec2e2cd
Sam Nau <2samnau@gmail.com>
2019-02-09 14:18:38 -0600

    * foundation for a battle scene * basic health incrementer/decrementer

---
+++ project/Assets/Hero_Health_Value.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hero_Health_Value : MonoBehaviour {
+    int healthValue = 0;
+    Text textbox;
+	// Use this for initialization
+	void Start () {
+        textbox = gameObject.GetComponent<Text>();
+        textbox.text = healthValue.ToString();
+        var player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMotionController>().enabled = false;
+       // Debug.Log(textbox);
+        //textbox.GetComponent<Text>().text = healthValue;
+	}
+	
+    void UpdateHealthValue(bool increase = true)
+    {
+        if (increase)
+        {
+            healthValue++;
+        } else
+        {
+            healthValue--;
+        }
+        textbox.text = healthValue.ToString();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("down"))
+        {
+            UpdateHealthValue(false);
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            UpdateHealthValue();
+        }
+
+    }
+}
