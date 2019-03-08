b7b8f5689ee7f2309823a626e65967a21717b79a
Sam Nau <2samnau@gmail.com>
2019-03-08 13:16:35 -0600

    * added start of a damge and death state indicator on the player by change the color of all of the child sprites

--- project/Assets/Hero_Health_Value.cs
+++ project/Assets/Hero_Health_Value.cs
@@ -9,19 +9,22 @@ public class Hero_Health_Value : MonoBehaviour {
     public bool playerIsAlive = true;
     Text textbox;
     Camera mainCamera;
+    GameObject player;
 
 	void Start () {
         mainCamera = Camera.main;
         healthValue = defaultHealth;
         textbox = gameObject.GetComponent<Text>();
         textbox.text = healthValue.ToString();
-        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMotionController>().inBattle = true;
+        player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<PlayerMotionController>().inBattle = true;
 	}
 	public void TakeDamage()
     {
         if(healthValue > 0)
         {
             healthValue--;
+            StartCoroutine(IndicateDamage());
         }
     }
     void UpdateHealthValue(bool increase = true)
@@ -31,7 +34,31 @@ public class Hero_Health_Value : MonoBehaviour {
         {
             textbox.text = healthValue.ToString() + " you died";
             playerIsAlive = false;
-            mainCamera.GetComponent<Camera_Shaker>().enabled = true;
+            IndicateDeath();
+            //mainCamera.GetComponent<Camera_Shaker>().enabled = true;
+        }
+    }
+    void IndicateDeath()
+    {
+        foreach (SpriteRenderer sr in player.GetComponentsInChildren<SpriteRenderer>())
+        {
+            Debug.Log(sr);
+            var deadColor = new Color(0, 0, 0, 0.5f);
+            sr.material.color = Color.Lerp(Color.white, deadColor, 1.5f);
+        }
+    }
+    IEnumerator IndicateDamage()
+    {
+        foreach (SpriteRenderer sr in player.GetComponentsInChildren<SpriteRenderer>())
+        {
+            Debug.Log(sr);
+            sr.material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        }
+        yield return new WaitForSeconds(0.1f);
+        foreach (SpriteRenderer sr in player.GetComponentsInChildren<SpriteRenderer>())
+        {
+            Debug.Log(sr);
+            sr.material.color = Color.white;
         }
     }
     void Update()
