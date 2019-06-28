7c99c91e82b7bc132cccbdf6d26578d17c23d478
Sam Nau <2samnau@gmail.com>
2019-06-28 12:46:06 -0500

    very beginning of key sequence manager

---
+++ project/Assets/scripts/Input_Sequence_Manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_Sequence_Manager : MonoBehaviour {
+    public string[] keySequence = { "1", "2", "3" };
+    int currentKeyIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void validateKeyPress()
+    {
+        var keyIsValid = correctKeyPressed();
+        if(keyIsValid)
+        {
+            Debug.Log("correct key pressed!");
+            currentKeyIndex++;
+        }
+    }
+
+    bool correctKeyPressed()
+    {
+        var currentKeyPressed = Input.inputString;
+        var validKeyPress = keySequence[currentKeyIndex];
+        return currentKeyPressed == validKeyPress;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown)
+        {
+            validateKeyPress();
+        }
+	}
+}
---
+++ project/Assets/scripts/Projectile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    GameObject familiar;
+    Vector2 currentPosition;
+    Vector2 targetPosition;
+    public bool KeepLooping = true;
+    public float speed = 0.25f;
+    float moveIncrement = 0;
+    GameObject monster;
+    monster_action_manager monsterManager;
+
+    void Start()
+    {
+        familiar = GameObject.FindGameObjectWithTag("familiar");
+        currentPosition = transform.position;
+        FindFamiliar();
+        monster = GameObject.FindGameObjectWithTag("Enemy");
+        monsterManager = monster.GetComponent<monster_action_manager>();
+    }
+
+    void FindFamiliar()
+    {
+        targetPosition = new Vector2(familiar.transform.position.x + 0.5f, familiar.transform.position.y + 3.25f);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("familiar"))
+        {
+            StartCoroutine(monsterManager.OpenDefenseWindow());
+        }
+    }
+    void findProjectilePosition()
+    {
+        currentPosition = transform.position;
+    }
+
+    void MoveProjectile()
+    {
+        moveIncrement += (Time.deltaTime * speed);
+        findProjectilePosition();
+        if(moveIncrement < 1.0f)
+        {
+            transform.position = Vector2.Lerp(currentPosition, targetPosition, moveIncrement);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        MoveProjectile();
+    }
+}
