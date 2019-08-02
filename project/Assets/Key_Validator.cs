14d0068cd2e9b136ddd02bf9537127b8264de9d9
Sam Nau <2samnau@gmail.com>
2019-08-02 13:07:15 -0500

    created basic key combo validator class

---
+++ project/Assets/Key_Validator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Key_Validator : MonoBehaviour
+{
+    public string[] keyCombo;
+    public string[] keyComboCopy;
+    string currentKeyToMatch;
+    float comboDelay = 0.5f;
+    bool withinWindow = false;
+    public bool comboPressed = false;
+
+    void validateAnyKeyInCombo()
+    {
+        for (int i = 0; i < keyCombo.Length; i++)
+        {
+            if (Input.GetKeyDown(keyCombo[i]))
+            {
+                Debug.Log(keyCombo.Where((val => val != keyCombo[i])).ToArray()[0]);
+               currentKeyToMatch = keyCombo.Where((val => val != keyCombo[i])).ToArray()[0];
+            }
+        }
+
+        if (currentKeyToMatch != null)
+        {
+            StartCoroutine(manageComboWindow());
+        }
+    }
+
+    void validateOtherComboHalf()
+    {
+        if (withinWindow)
+        {
+            comboPressed = Input.GetKeyDown(currentKeyToMatch);
+            Debug.Log(comboPressed);
+        }
+        currentKeyToMatch = null;
+    }
+
+    IEnumerator manageComboWindow()
+    {
+        withinWindow = true;
+        yield return new WaitForSeconds(comboDelay);
+        withinWindow = false;
+    }
+
+    void validateKeyPress()
+    {
+        if (keyCombo.Length == 0)
+        {
+            return;
+        }
+        if (currentKeyToMatch == null)
+        {
+            validateAnyKeyInCombo();
+        }
+        else
+        {
+            validateOtherComboHalf();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            validateKeyPress();
+        }
+    }
+}
--- project/Assets/scripts/Input_Sequence_Manager.cs
+++ project/Assets/scripts/Input_Sequence_Manager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Input_Sequence_Manager : MonoBehaviour {
     public string[] keySequence = { "1", "2", "3" };
+    public string[][] keyCombos;
+
     int currentKeyIndex = 0;
+
+    float comboDelay = 0.5f;
 
 	// Use this for initialization
 	void Start () {
-		
+        keyCombos[0] = new string[] { "a", "4" };
 	}
 	void validateKeyPress()
     {
@@ -22,6 +26,7 @@ public class Input_Sequence_Manager : MonoBehaviour {
 
     bool correctKeyPressed()
     {
+        var startTime = Time.time;
         var currentKeyPressed = Input.inputString;
         var validKeyPress = keySequence[currentKeyIndex];
         return currentKeyPressed == validKeyPress;
