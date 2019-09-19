0128bbaeca2d8db7dabdbc3a2bc3aea0e3626de3
Sam Nau <2samnau@gmail.com>
2019-09-19 13:13:56 -0500

    more spider idle animation progress

---
+++ project/Assets/scripts/LegIdleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegIdleController : MonoBehaviour {
+    public bool pauseIdle;
+    Animator legAnimator;
+    float startDelay;
+
+    // Use this for initialization
+    void Start() {
+        legAnimator = gameObject.GetComponent<Animator>();
+        startDelay = Random.Range(0.5f, 1.0f);
+        StartCoroutine("TriggerIdle");
+    }
+
+    IEnumerator TriggerIdle() {
+        Debug.Log("Idle start");
+       yield return new WaitForSeconds(startDelay);
+        legAnimator.SetBool("startIdle", true);
+        yield return new WaitForSeconds(2.0f);
+        legAnimator.SetBool("startIdle", false);
+        StartCoroutine("TriggerIdle");
+    }
+}
