c80cc561a2cafaf353d408fc04a401a39c4f7c9a
Sam Nau <2samnau@gmail.com>
2020-07-26 17:50:51 -0500

    * created simple animation state trigger script to set boolean triggers * added fire sheild animation sequencer script * removed the dummy timing frames from the fire sheild animtions

---
+++ project/Assets/scripts/FireShieldAnimationSequencer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireShieldAnimationSequencer : MonoBehaviour {
+    float ShieldRevealDelay = 1.5f;
+    Transform FireCore;
+    AnimationStateTrigger FlameTrigger;
+    Transform Flame;
+    AnimationStateTrigger CoreTrigger;
+    AnimationStateTrigger WrapperTrigger;
+	// Use this for initialization
+	void Start () {
+        FireCore = gameObject.transform.Find("fire_shield_core");
+        Flame = gameObject.transform.Find("fire_shield_flame");
+        CoreTrigger = FireCore.GetComponent<AnimationStateTrigger>();
+        FlameTrigger = Flame.GetComponent<AnimationStateTrigger>();
+        WrapperTrigger = gameObject.GetComponent<AnimationStateTrigger>();
+        StartCoroutine("TriggerFlameAppear");
+    }
+	
+     IEnumerator TriggerFlameAppear()
+    {
+        yield return new WaitForSeconds(ShieldRevealDelay);
+        FlameTrigger.TriggerChange = true;
+        yield return new WaitForSeconds(0.5f);
+        CoreTrigger.TriggerChange = true;
+        WrapperTrigger.TriggerChange = true;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
