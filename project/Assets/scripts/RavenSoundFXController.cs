e9ee543e66dcf67bf988d429d83edde7e4af61ce
Sam Nau <2samnau@gmail.com>
2020-12-30 22:04:00 -0600

    lots of new sound FX everywhere

---
+++ project/Assets/scripts/RavenSoundFXController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RavenSoundFXController : MonoBehaviour {
+    public AudioClip Thump;
+    public AudioClip StabStrike;
+    public AudioClip Hiss;
+    public AudioSource SoundSource;
+
+    void PlayThump()
+    {
+        PlayOneShot(Thump);
+    }
+
+    void PlayStabStrike()
+    {
+        PlayOneShot(StabStrike);
+    }
+
+    void PlayHiss()
+    {
+        PlayOneShot(Hiss);
+    }
+
+    void PlayOneShot(AudioClip TargetSound)
+    {
+        SoundSource.PlayOneShot(TargetSound);
+    }
+
+}
