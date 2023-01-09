7364b6bd01170a43c44cf2cd88f6fe087b88c3e7
LAPTOP-E1GQAVJS\samgn <2samnau@gmail.com>
2023-01-09 14:25:19 -0600

    - updated music player to compare the music clips before choosing what to destroy

--- project/Assets/scripts/HeroPositionManager.cs
+++ project/Assets/scripts/HeroPositionManager.cs
@@ -8,7 +8,6 @@ public class HeroPositionManager : MonoBehaviour
     Fade_Controller fadeController;
     ScenePosition scenePosition;
     PositionMarker[] positionMarkers;
-    // Start is called before the first frame update
     void Start()
     {
         fadeController = GameObject.FindObjectOfType<Fade_Controller>();
@@ -17,21 +16,12 @@ public class HeroPositionManager : MonoBehaviour
         string inputDirection = currentDirection == "up" || currentDirection == "down" ? scenePosition.lastDirection : currentDirection;
         InputStateTracker inputStateTracker = gameObject.GetComponent<InputStateTracker>();
         positionMarkers = GameObject.FindObjectsOfType<PositionMarker>();
-        print($"there are this many markers: {positionMarkers.Length}");
         PositionMarker targetMarker = positionMarkers.FirstOrDefault(positionMarker => positionMarker.direction.ToString() == currentDirection);
 
         if(targetMarker)
         {
-            //print($"target pos: {targetMarker.transform.position.x},{targetMarker.transform.position.y}");
-            print($"Molly faces: {inputDirection}");
             this.transform.position = targetMarker.transform.position;
             inputStateTracker.direction = inputDirection;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- project/Assets/scripts/MusicController.cs
+++ project/Assets/scripts/MusicController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MusicController : MonoBehaviour {
@@ -7,10 +8,22 @@ public class MusicController : MonoBehaviour {
     private void Awake()
     {
         GameObject[] musicPlayers = GameObject.FindGameObjectsWithTag("Music");
+        MusicController[] musicControllers = GameObject.FindObjectsOfType<MusicController>();
+        AudioSource audioSource = this.GetComponent<AudioSource>();
+        AudioClip clip = audioSource.clip;
+        bool clipMatch = false;
 
-        if(musicPlayers.Length > 1)
+        if(musicControllers.Length > 1)
         {
-            Destroy(this.gameObject);
+            clipMatch = musicControllers[0].GetComponent<AudioSource>().clip == musicControllers[1].GetComponent<AudioSource>().clip;
+            if(clipMatch)
+            {
+                Destroy(this.gameObject);
+            } else
+            {
+                GameObject musicToDestroy = musicControllers.FirstOrDefault(musicController => musicController.GetComponent<AudioSource>().clip != clip).gameObject;
+                Destroy(musicToDestroy);
+            }
         }
 
         DontDestroyOnLoad(this.gameObject);
