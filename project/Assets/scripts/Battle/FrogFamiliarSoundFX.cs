603b0c757fa40e84bdd75eb858885c640e6fb4a2
Sam Nau <samgnaw77@gmail.com>
2023-09-02 12:25:45 -0500

    Frog idle animation v1

---
+++ project/Assets/scripts/Battle/FrogFamiliarSoundFX.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogFamiliarSoundFX : SoundFXPlayer
+{
+    [SerializeField] AudioClip croak1;
+    [SerializeField] AudioClip croak2;
+    [SerializeField] AudioClip croak3;
+
+    List<AudioClip> croakList;
+
+    private void Start()
+    {
+        croakList = new List<AudioClip>();
+        if(croak1 != null && croak2 != null && croak3 != null)
+        {
+            croakList.Add(croak1);
+            croakList.Add(croak2);
+            croakList.Add(croak3);
+        }
+    }
+    public void PlayCroak()
+    {
+        int targetIndex = Random.Range(0, croakList.Count - 1);
+        AudioClip targetCroak = croakList[targetIndex];
+        PlayOneShot(targetCroak, .5f);
+    }
+}
