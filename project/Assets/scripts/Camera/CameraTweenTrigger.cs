424274626002ce50b1e1d7ee14c366ca51dbb186
Sam Nau <samgnaw77@gmail.com>
2024-11-18 11:50:48 -0600

    Custom castle wall dialog and new camera trigger script for camera motion triggering

--- project/Assets/scripts/Camera/CameraTweenTrigger.cs
+++ project/Assets/scripts/Camera/CameraTweenTrigger.cs
@@ -7,15 +7,12 @@ public class CameraTweenTrigger : MonoBehaviour
     Camera mainCamera;
     Vector3 startPostion;
     Vector3 targetPosition;
-    GameObject positionTrigger;
     GameObject positionTarget;
-    GameObject player;
-    BoxCollider2D triggerCollider;
     PositionTweener cameraTweener;
-    bool tweenTriggered = false;
-    bool triggerThrottled = false;
+    bool moveToTarget = true;
+    bool startPosSet = false;
     [SerializeField]
-    float tweenDuration = 2f;
+    float tweenDuration = 1.5f;
     void Start()
     {
         InitCameraTrigger();
@@ -28,69 +25,46 @@ public class CameraTweenTrigger : MonoBehaviour
 
     void InitCameraTrigger()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        //positionTrigger = GameObject.FindGameObjectWithTag("CameraTrigger");
         positionTarget = GameObject.FindGameObjectWithTag("CameraTarget");
         mainCamera = Camera.main;
-        startPostion = mainCamera.transform.position;
         targetPosition = positionTarget.transform.position;
         cameraTweener = mainCamera.GetComponent<PositionTweener>();
-        //triggerCollider = positionTrigger.GetComponent<BoxCollider2D>();
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    print("camera tween was triggered");
-    //    ToggleCameraTween();
-    //    //if (collision.CompareTag("Player"))
-    //    //{
-    //    //    print("camera tween was triggered");
-    //    //    ToggleCameraTween();
-    //    //}
-    //}
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player") && collision.isTrigger && !tweenTriggered)
+        if (collision.CompareTag("Player") && collision.isTrigger)
         {
             if (mainCamera.GetComponent<Animator>() != null)
             {
                 mainCamera.GetComponent<Animator>().enabled = false;
             }
+
             ToggleCameraTween();
-            tweenTriggered = true;
         }
     }
-
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && collision.isTrigger && tweenTriggered)
+        if (collision.CompareTag("Player") && collision.isTrigger)
         {
-            //if(mainCamera.GetComponent<Animator>() != null)
-            //{
-            //    mainCamera.GetComponent<Animator>().enabled = true;
-            //}
-            //print(collision.GetComponent<BoxCollider2D>()?.size.y);
-            tweenTriggered = false;
+            ToggleCameraTween();
         }
-    }
-
-    IEnumerator ThrottleTrigger()
-    {
-        triggerThrottled = true;
-        yield return new WaitForSeconds(tweenDuration * 2f);
-        triggerThrottled = false;
     }
 
     void ToggleCameraTween()
     {
-        Vector3 targetTweenPosition = tweenTriggered ? startPostion : targetPosition;
+        cameraTweener.StopAllCoroutines();
+        if (!startPosSet)
+        {
+            var mainPos = mainCamera.transform.position;
+            startPostion = mainPos;
+            startPosSet = true;
+        }
+        
+        Vector3 targetTweenPosition = moveToTarget ? targetPosition : startPostion;
         cameraTweener.TriggerPositionByDuration(targetTweenPosition, tweenDuration);
+        moveToTarget = !moveToTarget;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
